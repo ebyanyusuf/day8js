03473bd0a82e1c41a7eaea45cb146541a7996b54
ebyanyusuf <ebyan.yusuf@gmail.com>
2021-07-11 18:04:24 -0400

    first

---
+++ day8.js
@@ -0,0 +1,19 @@
+function regexVar() {
+    // (Mr\.|Mrs\.|Ms\.|Dr\.|Er\.) Word must start with either one
+    // . Match any characters except line breaks
+    // * Match 0 or more after this
+    // [a-zA-Z] any letters in the alphabet upper or lowercase
+    // $ ends with the preceding arguments [a-zA-Z]
+    // /g globally match
+    // m multiline match   
+    const re = new RegExp(/(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.).*[a-zA-Z]$/gm);
+    
+    return re;
+}
+// Given
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
